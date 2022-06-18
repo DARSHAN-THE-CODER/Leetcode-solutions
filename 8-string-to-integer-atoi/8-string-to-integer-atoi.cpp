031214e5e57a5e9a21b38795308bae8b8095b637
Darshan V <79416723+DARSHAN-THE-CODER@users.noreply.github.com>
2022-06-18 21:29:19 +0530

    Time: 6 ms (43.50%), Space: 7.8 MB (5.62%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -1,37 +1,55 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int sign = 1; 
-        int ans = 0; 
-        int i = 0;
-        int n = s.size();
-        
-        
-        while (i < n && s[i] == ' ') { 
-            i++; 
-        }
-        
-        if (i < n && s[i] == '+') {
-            sign = 1;
-            i++;
-        } 
-        else if (i < n && s[i] == '-') {
-            sign = -1;
-            i++;
-        }
-
-        while (i < n && isdigit(s[i])) {
-            int sol = s[i] - '0';
-
-
-            if ((ans > INT_MAX / 10) || (ans == INT_MAX / 10 && sol > INT_MAX % 10))                    { 
-                  
-                return sign == 1 ? INT_MAX : INT_MIN;
-            }
+        int i=0;
+        int num;
+        bool x=true;
+        string z;
+        if(s=="" || s=="-" || s==" " || s=="+")
+            return 0;
+        while(x && i<s.length())
+        {
+            if(isdigit(s[i]) || s[i]=='+')
+            {
+                
+                if(i>0 && s[i-1]=='-')
+                {
+                        z=z+'-';
+                }               
+                while(isdigit(s[i]) && i<s.length() && s[i]!='.' && s[i]!=' ' || s[i]=='+')
+                {
+                    
+                        z=z+s[i];
+                        x=false;
+                        i++;
+                    
+                }
+                }
             
-            ans = 10 * ans + sol;
-            i++;
+            if((x && isalpha(s[i])) || ( x && s[i]=='.') || (x && s[i]=='+'))  
+            {
+                cout<<"s[i] "<<s[i]<<endl;
+                z='0';
+                x=false;
             }
-        return sign * ans;
+            if(s[i]=='-' && s[i+1]=='-') return 0;
+            i++;
+        }
+
+        try{
+            return stoi(z);
+        }
+        catch(std::out_of_range &e)
+        {
+            stringstream ss; 
+            ss << z;
+            ss >> num;
+        }
+        catch(...)
+        {
+            cout<<"i am unexpected "<<endl;
+            return 0;
+        }
+        return num;
     }
 };
