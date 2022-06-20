7fab41895e1b874bf1e88f1f3e5353e96bf5561f
Darshan V <79416723+DARSHAN-THE-CODER@users.noreply.github.com>
2022-06-20 14:59:09 +0530

    Time: 48 ms (5.61%), Space: 6 MB (31.89%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -4,19 +4,16 @@ public:
         
         long long r_num=0;
         long long num=abs(x);
-        if(x>INT_MAX || x<INT_MIN)
+        if(x>INT_MAX || x<INT_MIN || abs(x)!=x)
             return false;
+       
         while(num!=0)
         {
             long long digit=num%10;
             r_num=r_num*10+digit;
             num=num/10;
         }
-        
-        
-        if(abs(x)!=x)
-            return false;
-
+                
         for(int i=0;i<to_string(x).length();i++)
         {
             long long digit2=r_num%10;
