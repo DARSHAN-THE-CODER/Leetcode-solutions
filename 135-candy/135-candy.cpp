a154028db7cbed617a29be9236c38f5cc7ffc3f0
Darshan V <79416723+DARSHAN-THE-CODER@users.noreply.github.com>
2022-07-04 16:01:04 +0530

    Time: 36 ms (43.40%), Space: 17.7 MB (84.26%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -7,14 +7,11 @@ public:
         
         for(int i=0;i<n-1;i++)
         {
-            if(ratings[i+1]>ratings[i])
-              candies[i+1]=candies[i]+1;
+            ratings[i+1]>ratings[i] ?( candies[i+1]=candies[i]+1) : ' ';
         }
         for(int i=n-2;i>=0;i--)
         {
-            if(ratings[i]>ratings[i+1] && candies[i]<=candies[i+1])
-                candies[i]=candies[i+1]+1;
-                
+            (ratings[i]>ratings[i+1] && candies[i]<=candies[i+1]) ? (candies[i]=candies[i+1]+1) : ' ';                
         }
         
         int sum=accumulate(candies.begin(),candies.end(),0);
