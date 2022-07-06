ca1fc9e84d8090d8e052f4fe78ee4eaea7c62e41
Darshan V <79416723+DARSHAN-THE-CODER@users.noreply.github.com>
2022-07-06 13:23:19 +0530

    Time: 19 ms (22.24%), Space: 5.8 MB (96.23%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int fib(int n) {
+        int f=0;
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        else
+            return fib(n-1)+fib(n-2);
+    }
+};
