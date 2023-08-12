36af6c5685ab3cd59e9db0b2794d6cf41d58aca0
Darshan V <79416723+DARSHAN-THE-CODER@users.noreply.github.com>
2023-08-12 18:07:15 +0530

    Time: 8 ms (91.86%) | Memory: 18.8 MB (24.33%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    
-    int change(int amount, vector<int>& coins) {
-        vector<int> dp(amount + 1, 0);
-        dp[0] = 1;
-        
-        for (int coin : coins) {
-            for (int j = coin; j <= amount; j++) {
-                dp[j] += dp[j - coin];
-            }
+    int helper(int index, int amount, vector<vector<int>> &dp, vector<int>& coins){
+        if(amount == 0) return 1;
+        if(index<0 || amount<0) return 0;
+        if(index == 0){
+            return (amount % coins[0] == 0);
         }
-        
-        return dp[amount];
+        if(dp[index][amount] != -1) return dp[index][amount];
+        int pick=0, notPick;
+        notPick=helper(index-1, amount, dp, coins);
+        if(coins[index]<=amount){
+            pick=helper(index, amount-coins[index], dp, coins);
+        }
+        return dp[index][amount] = pick + notPick;
+    }
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(amount+1, -1));
+        return helper(n-1, amount, dp, coins);
     }
 };
