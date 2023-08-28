29bf81fff79df636ee01838520cf2ab089555040
Darshan V <79416723+DARSHAN-THE-CODER@users.noreply.github.com>
2023-08-28 21:08:47 +0530

    Time: 4 ms (68.88%) | Memory: 7.6 MB (57.42%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
+    bool helper(int index, vector<int> &piles, vector<int> &dp){
+        if(index<0) return false;
+        if(index == 0) return piles[index];
+        if(dp[index]!= -1) return dp[index];
+
+        int take = helper(index-1, piles, dp) + piles[index];
+        int notTake = helper(index-2, piles, dp) + 0;
+
+        return dp[index] = max(take, notTake);
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        vector<int> dp(n+1, -1);
+
+        return max(helper(n-1, piles, dp), helper(0, piles, dp));
     }
 };
