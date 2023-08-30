82f7efdc839035d2c1f4ae4b8119dbfbfcc47fbd
Darshan V <79416723+DARSHAN-THE-CODER@users.noreply.github.com>
2023-08-30 13:28:55 +0530

    Time: 148 ms (64.49%) | Memory: 29.9 MB (39.72%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -6,27 +6,30 @@ public:
 
         int n = nums.size();
 
-        for(int i=0;i<n-2; i++){
-            if(i==0 || (i>0 && nums[i] != nums[i-1])){
+        for(int i=0;i<n-2;i++){
+            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
                 int low = i+1, high = n-1, sum = 0 - nums[i];
-
-                while(low<high){
-                    if(nums[low]+nums[high] == sum){
+                while(low < high){
+                    if(nums[low] + nums[high] == sum){
                         vector<int> temp;
                         temp.push_back(nums[low]);
                         temp.push_back(nums[i]);
-                        temp.push_back(nums[high]);   
+                        temp.push_back(nums[high]);
 
                         ans.push_back(temp);
-                        while(low<high && nums[low]==nums[low+1]) low++;
-                        while(low<high &&  nums[high]==nums[high-1]) high--;
 
+                        while(low<high && nums[low]==nums[low+1]) low++;
+                        while(low<high && nums[high]==nums[high-1]) high--;
                         low++;
-                        high--;                     
+                        high--;
+                    }else {
+                        if(low<high && nums[low]+nums[high] < sum){
+                            low++;
+                        }else{
+                            high--;
+                        }
                     }
-                    else if(low<high && nums[low]+nums[high] < sum) low++;
-                    else high--;
-                } 
+                }
             }
         }
         return ans;
