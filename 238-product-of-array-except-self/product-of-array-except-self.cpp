d26eb3509c2224e072c3b85002e0c1ae5e8ee93a
Darshan V <79416723+DARSHAN-THE-CODER@users.noreply.github.com>
2023-08-30 14:32:12 +0530

    Time: 84 ms (5.52%) | Memory: 25.1 MB (16.10%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,13 +4,18 @@ public:
         int n=nums.size();
         vector<int>lp(n,1);
         vector<int>rp(n,1);
+        cout<<"lp"<<endl;
         for(int i=1;i<n;i++)
         {
             lp[i]=lp[i-1]*nums[i-1];
+            cout<<lp[i]<<" ";
         }
+        cout<<endl<<"rp"<<endl;
         for(int i=n-2;i>=0;i--)
         {
             rp[i]=rp[i+1]*nums[i+1];
+            cout<<rp[i]<<" ";
+
         }
         vector<int>ans(n,1);
         for(int i=0;i<n;i++)
